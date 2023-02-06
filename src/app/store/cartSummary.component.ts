a09c583312df7c5c3e05ac778d381c6bbc485053
Paulo Cescar <paulo.cr93@gmail.com>
2023-02-06 00:38:55 -0300

    [FIX] TopBar correction

--- src/app/store/cartSummary.component.ts
+++ src/app/store/cartSummary.component.ts
@@ -3,7 +3,8 @@ import { Cart } from "../model/cart.model";
 
 @Component({
     selector: "cart-summary",
-    templateUrl: "cartSummary.component.html"
+    templateUrl: "cartSummary.component.html",
+    styleUrls: ['./store.component.css']
 })
 export class CartSummaryComponent {
     constructor(public cart: Cart) { }
